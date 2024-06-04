1a1c7760b098d17ba7daab22a9ddf8dff94f2760
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-06-04 17:13:22 +0200

    some slight code modifications

--- loratx.py
+++ loratx.py
@@ -4,40 +4,25 @@ import redis
 import serial
 
 
-def fetch_redis_string(ieee, hash):
+def fetch_redis_string(ieee: str, hash: str) -> str | None:
     command = "GETDEL lorabridge:device:{}:message:{}".format(
         ieee.decode("utf-8"), hash.decode("utf-8")
     )
     print(command)
-    reply = redis_client.execute_command(command)
-
-    if reply:
+    if reply := redis_client.execute_command(command):
         return reply
-    else:
-        return None
 
 
-def fetch_redis_queues():
-    reply = redis_client.smembers("lorabridge:device:index")
-
-    if reply:
+def fetch_redis_queues() -> str | None:
+    if reply := redis_client.smembers("lorabridge:device:index"):
         return reply
-    else:
-        return None
 
 
-def fetch_redis_message(ieee):
+def fetch_redis_message(ieee: str) -> str | None:
     command = "ZPOPMIN lorabridge:queue:{}".format(ieee.decode("utf-8"))
-    # print(command)
-    reply = redis_client.execute_command(command)
-
-    if reply:
+    if reply := redis_client.execute_command(command):
         return reply
-    else:
-        return None
 
-
-# Assuming redis_client is already initialized
 
 redis_client = redis.Redis(
     host=os.environ.get("REDIS_HOST", "localhost"),
@@ -46,24 +31,18 @@ redis_client = redis.Redis(
 )
 
 
-def fetch_one_message():
+def fetch_one_message() -> str | None:
     redis_queues = fetch_redis_queues()
 
-    if redis_queues == None:
-        return None
-
-    for redis_queue in redis_queues:
-        redis_msg = fetch_redis_message(redis_queue)
-        if redis_msg == None:
-            continue
-        redis_string = fetch_redis_string(redis_queue, redis_msg[0])
-
-        return redis_string
-        break
-    return None
+    if redis_queues != None:
+        for redis_queue in redis_queues:
+            redis_msg = fetch_redis_message(redis_queue)
+            if redis_msg == None:
+                continue
+            return fetch_redis_string(redis_queue, redis_msg[0])
 
 
-def push_to_command_queue(lb_command: str):
+def push_to_command_queue(lb_command: str) -> None:
     redis_client.lpush("lbcommands", lb_command)
 
 
@@ -78,28 +57,22 @@ def main():
     ser = serial.Serial(serial_port, baudrate)
 
     while True:
-        try:
+        # Read data from serial port
+        data = ser.readline().decode("utf-8").strip()
+        print(data)
 
-            # Read data from serial port
-            data = ser.readline().decode("utf-8").strip()
-            print(data)
+        if "LBDATA" in data:
+            print("Data: ", data[8:], " being pushed onto command stack")
+            push_to_command_queue(data[8:])
 
-            if "LBDATA" in data:
-                print("Data: ", data[8:], " being pushed onto command stack")
-                push_to_command_queue(data[8:])
-
-            # Transmit "tx_ok" back to the serial interface
-            if "tx_token" in data:
-                lb_message = fetch_one_message()
-                if lb_message != None:
-                    ser.write(lb_message)
-                    print("Sent a message")
-                else:
-                    print("Queue empty, sending nothing...")
-        except KeyboardInterrupt:
-            print("Exiting...")
-            ser.close()
-            break
+        # Transmit "tx_ok" back to the serial interface
+        if "tx_token" in data:
+            lb_message = fetch_one_message()
+            if lb_message != None:
+                ser.write(lb_message)
+                print("Sent a message")
+            else:
+                print("Queue empty, sending nothing...")
 
 
 if __name__ == "__main__":
